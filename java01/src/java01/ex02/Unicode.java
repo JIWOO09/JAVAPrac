19a8967caaff21c5d45f1ab1be6a98c30a531171
JIWOO <s__009@naver.com>
2021-10-07 18:46:33 +0900

    21.10.07 자바 복습 1

---
+++ java01/src/java01/ex02/Unicode.java
@@ -0,0 +1,15 @@
+package java01.ex02;
+
+public class Unicode {
+
+	public static void main(String[] args) {
+
+		char ch1 = '헐';
+		char ch2 = '확';
+		char ch3 = 54736; //문자 헐의 유니코드 값
+		char ch4 = 54869; // 문자 확의 유니코드 값 
+		System.out.println(ch1 +" "+ ch2);
+		System.out.println(ch3 +" "+ ch4);
+	}
+
+}
---
+++ java01/src/java01/ex03/Boolean.java
@@ -0,0 +1,17 @@
+package java01.ex03;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		int i1 = 10;
+		int i2 = 20;
+		
+		System.out.println(i1 > i2);
+		System.out.println(i1 < i2);
+	}
+
+}
