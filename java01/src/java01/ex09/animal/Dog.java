0b8d09ab455c9ec80abeb06638a05e7179c9a443
JIWOO <s__009@naver.com>
2021-10-24 18:34:02 +0900

    2021.10.24 public, default 접근제한자

--- java01/src/java01/ex09/animal/Dog.java
+++ java01/src/java01/ex09/animal/Dog.java
@@ -1,5 +1,11 @@
 package java01.ex09.animal;
 
-public class Dog {
 
+
+public class Dog {
+    public void welcom(java01.ex09.zoo.Cat c) {
+    					//파일 이름명 그대로 점.으로 불러오기 
+        c.makeSound();     // 호출 가능! 컴파일 성공!
+       // c.makeHappy();     // 호출 불가! 컴파일 오류! -> default로 선언 되어있기 때문에
+    }
 }
