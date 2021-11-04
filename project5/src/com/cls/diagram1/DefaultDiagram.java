e19af0e25dec1fafd98ff7073fd7a818427836f4
JIWOO <s__009@naver.com>
2021-11-04 12:08:31 +0900

    2021.11.04 project5부터 레파지토리 새로 만들어서 연동

---
+++ project5/src/com/cls/diagram1/DefaultDiagram.java
@@ -0,0 +1,36 @@
+package com.cls.diagram1;
+
+
+	//클래스다이어그램 샘플용 코드
+public class DefaultDiagram {
+
+	public int x1;
+	int x2 = 10;
+	protected String s1;
+	private String s2 = "Diagram";
+	
+	public DefaultDiagram() {
+		// 기본 생성자.
+	}
+	
+	public DefaultDiagram(int x) {
+		// 매개변수가 있는 생성자.
+	}
+	
+	public void method1() {
+		// 메서드.
+	}
+	
+	void method2(int x) {
+		// 메서드.
+	}
+	
+	protected String method3() {
+		return s1;
+	}
+	
+	private String method4(String s) {
+		return s2;
+	}
+	
+}
