439643ca25bb4437f0b189622890aca50e577f3f
JIWOO <s__009@naver.com>
2021-11-16 14:26:08 +0900

    2021.11.16 로그인(id,pass,email) 페이지 만들기

---
+++ project6/src/main/java/com/web/join/model/JoinDTO.java
@@ -0,0 +1,63 @@
+package com.web.join.model;
+
+public class JoinDTO {
+	
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	
+	
+	public JoinDTO() {
+		this("", "", ""); 
+	}
+	
+	public JoinDTO(String username) {
+		this.username = username;
+	} //username 담아내는 생성자
+	
+	public JoinDTO(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public JoinDTO(int id, String username, String password) {
+		this(username, password);
+		this.id = id;
+	}
+	
+	public JoinDTO(String username, String password, String email) {
+		this(username, password);
+		this.email = email;
+	}
+	
+	public JoinDTO(int id, String username, String password, String email) {
+		this(username, password, email);
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassowrd(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ project6/src/main/webapp/static/js/join.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+window.onload = function() {
+	document.getElementById("id_pass2").addEventListener("blur", passwordCheck);
+}
+
+function passwordCheck() {//비밀번호가 일치하는지 
+	var password = document.getElementById("id_pass1");
+	if(this.value === password.value) {
+		this.nextElementSibling.firstChild.innerText = "올바른 입력값 입니다.";
+	} else {
+		this.nextElementSibling.firstChild.innerText = "패스워드를 다시 확인 하세요.";
+	}
+}	
