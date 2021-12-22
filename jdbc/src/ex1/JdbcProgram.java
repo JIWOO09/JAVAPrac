8a3089e9247f8294cc6a2cf70e94044883c70b1e
JIWOO <s__009@naver.com>
2021-12-22 20:00:42 +0900

    2021.12.22 JDBC 연습2

--- jdbc/src/ex1/JdbcProgram.java
+++ jdbc/src/ex1/JdbcProgram.java
@@ -5,14 +5,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class JdbcProgram {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
 		
 		String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE"; //테이블 조회
+		//String sql = "SELECT * FROM NOTICE"; //테이블 조회
 		
+		//데이터 가공처리는 SQL에서 하는것 -> 변수 중복 되니 위에는 주석처리
+		String sql = "SELECT * FROM NOTICE WHERE HIT > 10";
+
 		//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		//첫번째 로드 객체 생성
@@ -29,9 +33,34 @@ public class JdbcProgram {
 		
 		
 		//오류를 발생하지 않으려면 if문
-		if(rs.next()) { //가져오고 다음 레코드로 커서 이동
-			String title = rs.getString("TITLE"); //타이틀 컬럼 주세요
-			System.out.println(title);
+//		if(rs.next()) { //가져오고 다음 레코드로 커서 이동
+//			int id = rs.getInt("Id"); //아이디 주세요
+//			String title = rs.getString("TITLE"); //타이틀 주세요
+//			String writerid = rs.getString("WRITER_ID");//작성자 주세요
+//			Date regDate =rs.getDate("REGDATE");//날짜 주세요
+//			String content = rs.getString("CONTENT"); //내용 주세요
+//			int hit = rs.getInt("HIT"); //추천수 주세요
+//			
+//			System.out.printf("id: %d, title :%s, writerid: %s,regDate: %s, content: %s, hit: %d\n",
+//								id, title, writerid, regDate, content, hit);
+//		}
+		
+		//반복문 while -> 빈 레코드가 나올 때까지 반복하는 false를 만나면 멈춘다.
+		while(rs.next()) { //가져오고 다음 레코드로 커서 이동
+			int id = rs.getInt("Id"); //아이디 주세요
+			String title = rs.getString("TITLE"); //타이틀 주세요
+			String writerid = rs.getString("WRITER_ID");//작성자 주세요
+			Date regDate =rs.getDate("REGDATE");//날짜 주세요
+			String content = rs.getString("CONTENT"); //내용 주세요
+			int hit = rs.getInt("HIT"); //조회수 주세요
+			
+			//조회수가 10인 이상만 출력
+			//내가 풀어본것 if(rs.getInt(hit) >= 10) {
+			
+			
+			System.out.printf("id: %d, title :%s, writerid: %s,regDate: %s, content: %s, hit: %d\n",
+								id, title, writerid, regDate, content, hit);
+		
 		}
 		//자원 끊을때는 반대로 실행
 		rs.close();
