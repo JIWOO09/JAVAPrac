6ff958ad756d6ac8a2e70b4afd619d35df69c1e3
JIWOO <s__009@naver.com>
2022-01-08 22:49:20 +0900

    2022.01.08 GET 기본값 설정

--- JspProject/src/main/java/com/jiwoo/web/Example.java
+++ JspProject/src/main/java/com/jiwoo/web/Example.java
@@ -20,7 +20,18 @@ public class Example extends HttpServlet{
 		response.setContentType("text/html; charset=UTF-8"); //받은 정보를 어떻게 해석할것인지
 								//html을 UTF-8로 해석해!
 		PrintWriter out = response.getWriter();
-		for(int i = 0; i < 100; i++)
+		
+		//null 에러가 나오지 않도록
+		//유효하지 않는 임시변수에 담기 (이름 뒤에 언더바 사용)
+		String count_ = request.getParameter("count");
+		
+		int count = 100;//기본값 셋팅
+		//null이 아니면 | 빈문자열이 아니면 : 임시변수를 기본값으로 대체한다.
+		if(count_ != null && !count_.equals(""))
+			count = Integer.parseInt(count_);
+			//임시변수를 기본값으로 대체한다.
+		
+		for(int i = 0; i < count; i++)
 		{
 		out.println((i+1) + ": 안녕 Servlet<br>");
 		}
