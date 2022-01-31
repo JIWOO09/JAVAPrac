46f1bf1446d3cf923e8f8625fa161757b2a69d59
JIWOO <s__009@naver.com>
2022-01-31 17:33:30 +0900

    2022.01.31 Notice Service Class

--- JspProject/src/main/java/com/jiwoo/web/controller/NoticeListController.java
+++ JspProject/src/main/java/com/jiwoo/web/controller/NoticeListController.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.jiwoo.web.entity.Notice;
+import com.jiwoo.web.service.NoticeService;
 
 
 @WebServlet("/notice/list")
@@ -24,61 +25,10 @@ public class NoticeListController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+		NoticeService service = new NoticeService();
 		//notice 객체가 여러개 필요하기 때문에 
-		List<Notice> list = new ArrayList<>();
-		
-		String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE"; //테이블 조회
-		
-		try {
-			//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			//첫번째 로드 객체 생성
-			//메모리상에 드라이버가 올라감
-			Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
-			//두번째 연결 객체 생성
-			//연결 되면 객체 참조
-			Statement st = con.createStatement();
-			//con으로 이어 받아 세번째 실행 객체 생성
-			//사용자로부터 요구 받은 쿼리 실행
-			ResultSet rs = st.executeQuery(sql);
-
-
-							
-			while(rs.next()) {
-				int id = rs.getInt("ID");
-				String title = rs.getString("TITLE");
-				Date regdate = rs.getDate("REGDATE");
-				String writerid = rs.getString("WRITER_ID");
-				String hit = rs.getString("HIT");
-				String files = rs.getString("FILES");
-				String content = rs.getString("CONTENT");
+		List<Notice> list = service.getNoticeList();
 				
-				//notice객체그릇에 데이터(속성들)담기, 생성자와 순서 동일하게
-				Notice notice = new Notice(
-									id,
-									title,
-									regdate,
-									writerid,
-									hit,
-									files,
-									content
-									);
-				
-				//반복문 실행시 객체에 추가
-				list.add(notice);
-				
-				}
-		    	rs.close();
-		    	st.close();
-		        con.close();
-		        
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
 		//전달 전 request에 담기
 		request.setAttribute("list", list);
 		
--- JspProject/src/main/java/com/jiwoo/web/service/NoticeService.java
+++ JspProject/src/main/java/com/jiwoo/web/service/NoticeService.java
@@ -1,5 +1,13 @@
 package com.jiwoo.web.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.jiwoo.web.entity.Notice;
@@ -23,14 +31,76 @@ public class NoticeService {
 	
 	//메소드 이름이 같은 경우 중복되니까 -> 인자가 많은 메소드만 살려서 구현
 	//제목, 이름, 등으로 검색했을 때
-	public List<Notice> getNoticeList(String field, String query, int page) {
+	public List<Notice> getNoticeList(String field/*TITLE, WRITER_ID*/, String query/*A*/, int page) {
+		
+		//controller에 있던 코드 가져오기
+		//notice 객체가 여러개 필요하기 때문에 
+		List<Notice> list = new ArrayList<>();
 		
 		//SQL문장을 이걸 쓸것이다
 		String sql = "SELECT * FROM (" +
-				"	SELECT ROWNUM NUM, N. *" +
-				"	FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N"+
+				"	SELECT ROWNUM NUM, N. *" +		//TITLE	   //패턴비교
+				"	FROM (SELECT * FROM NOTICE WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N"+
 				"	) " +
-				"	WHERE NUM BETWEEN 6 AND 10; ";
+				"	WHERE NUM BETWEEN ? AND ? ";
+				//1, 11, 21,31 ..->an = 1+(page-1)*10
+				//10, 20, 30..->page*10
+		
+				String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
+				
+				try {
+					//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+					//첫번째 로드 객체 생성
+					//메모리상에 드라이버가 올라감
+					Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
+					//두번째 연결 객체 생성
+					//연결 되면 객체 참조
+					//Statement st = con.createStatement();
+					//con으로 이어 받아 세번째 실행 객체 생성
+					//사용자로부터 요구 받은 쿼리 실행
+					PreparedStatement st = con.prepareStatement(sql);
+					st.setString(1, "%"+query+"%");
+					st.setInt(2, 1+(page-1)*10);
+					st.setInt(3, page*10);
+					
+					ResultSet rs = st.executeQuery();
+
+
+									
+					while(rs.next()) {
+						int id = rs.getInt("ID");
+						String title = rs.getString("TITLE");
+						Date regdate = rs.getDate("REGDATE");
+						String writerid = rs.getString("WRITER_ID");
+						String hit = rs.getString("HIT");
+						String files = rs.getString("FILES");
+						String content = rs.getString("CONTENT");
+						
+						//notice객체그릇에 데이터(속성들)담기, 생성자와 순서 동일하게
+						Notice notice = new Notice(
+											id,
+											title,
+											regdate,
+											writerid,
+											hit,
+											files,
+											content
+											);
+						
+						//반복문 실행시 객체에 추가
+						list.add(notice);
+						
+						}
+				    	rs.close();
+				    	st.close();
+				        con.close();
+				        
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 		
 		return null;
 	}
@@ -41,40 +111,236 @@ public class NoticeService {
 	}
 	//현재 페이지 알려주는 숫자 1/5 page 이런식
 	public int getNoticeCount(String field, String query) {
+		
+		//기본값 설정
+		int count = 0;
+		
 		//SQL문장을 이걸 쓸것이다
-		String sql = "SELECT * FROM (" +
+		String sql = "SELECT COUNT(ID) COUNT FROM (" +
 				"	SELECT ROWNUM NUM, N. *" +
-				"	FROM (SELECT * FROM NOTICE ORDER BY REGDATE DESC) N"+
-				"	) " +
-				"	WHERE NUM BETWEEN 6 AND 10;";
-		return 0;
+				"	FROM (SELECT * FROM NOTICE WHERE "+field+" LIKE ? ORDER BY REGDATE DESC) N"+
+				"	) ";
+		
+		String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+		try {
+			//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//첫번째 로드 객체 생성
+			//메모리상에 드라이버가 올라감
+			Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
+			//두번째 연결 객체 생성
+			//연결 되면 객체 참조
+			//Statement st = con.createStatement();
+			//con으로 이어 받아 세번째 실행 객체 생성
+			//사용자로부터 요구 받은 쿼리 실행
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, "%"+query+"%");
+			
+			ResultSet rs = st.executeQuery();
+
+			//목록이 아닌 집계 카운트값
+			count = rs.getInt("count");				
+			
+		    	rs.close();
+		    	st.close();
+		        con.close();
+		        
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return count;
 	}
 	
 	//공시사항 자세한 페이지 -> ID로 받기
 	public Notice getNotice(int id) {
-		String sql = "SELECT * FROM nOTICE WHERE ID=?";
 		
-		return null;
+		Notice notice = null;
+		String sql = "SELECT * FROM NOTICE WHERE ID=?";
+		
+		String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+		try {
+			//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//첫번째 로드 객체 생성
+			//메모리상에 드라이버가 올라감
+			Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
+			//두번째 연결 객체 생성
+			//연결 되면 객체 참조
+			//Statement st = con.createStatement();
+			//con으로 이어 받아 세번째 실행 객체 생성
+			//사용자로부터 요구 받은 쿼리 실행
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1,id);
+			
+			
+			ResultSet rs = st.executeQuery();
+
+
+							
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerid = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				//notice객체그릇에 데이터(속성들)담기, 생성자와 순서 동일하게
+				notice = new Notice(
+									nid,
+									title,
+									regdate,
+									writerid,
+									hit,
+									files,
+									content
+									);
+				
+				}
+		    	rs.close();
+		    	st.close();
+		        con.close();
+		        
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		
+		return notice;
 	}
 	
 	//다음글 -> ID로 받기 : 다음이 뭔지 알아야한다
 	//ID가 3인 현재글에서 다음글 가져오기 , 
 	//목록은 계속해서 수정되기 때문에 날짜로 기준 잡아서  -> 현재보다 큰 날짜면 ROWNUM1인 다음글 
 	public Notice getNextNotice(int id) {
+		Notice notice = null;
 		String sql = "SELECT * FROM NOTICE WHERE ID = (" +
 			    "	SELECT ID FROM NOTICE " +
-			    "	WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID =13) " +
+			    "	WHERE REGDATE > (SELECT REGDATE FROM NOTICE WHERE ID =?) " +
 			    "	AND ROWNUM = 1 " +
 			    "	)";
-		return null;
+String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+		try {
+			//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//첫번째 로드 객체 생성
+			//메모리상에 드라이버가 올라감
+			Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
+			//두번째 연결 객체 생성
+			//연결 되면 객체 참조
+			//Statement st = con.createStatement();
+			//con으로 이어 받아 세번째 실행 객체 생성
+			//사용자로부터 요구 받은 쿼리 실행
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1,id);
+			
+			
+			ResultSet rs = st.executeQuery();
+
+
+							
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerid = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				//notice객체그릇에 데이터(속성들)담기, 생성자와 순서 동일하게
+				notice = new Notice(
+									nid,
+									title,
+									regdate,
+									writerid,
+									hit,
+									files,
+									content
+									);
+				
+				}
+		    	rs.close();
+		    	st.close();
+		        con.close();
+		        
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return notice;
 	}
 	//이전글 -> ID로 받기 : 이전이 뭔지 알아야한다
 	public Notice gePrevtNotice(int id) {
+		Notice notice = null;
 		String sql = "SELECT * FROM NOTICE WHERE ID = (" +
 				"	SELECT ID FROM NOTICE" +  
-			    "	WHERE REGDATE < (SELECT REGDATE FROM NOTICE WHERE ID =13)"+
+			    "	WHERE REGDATE < (SELECT REGDATE FROM NOTICE WHERE ID =?)"+
 			    "	AND ROWNUM = 1";
-		return null;
+		
+String url ="jdbc:oracle:thin:@localhost:1521/xepdb1";
+		
+		try {
+			//총 4개의 객체를 생성해야한다(new로 객체 생성하지 않음) -> 거의 바뀌지 않음 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//첫번째 로드 객체 생성
+			//메모리상에 드라이버가 올라감
+			Connection con = DriverManager.getConnection(url,"NEWJDBC","1234");
+			//두번째 연결 객체 생성
+			//연결 되면 객체 참조
+			//Statement st = con.createStatement();
+			//con으로 이어 받아 세번째 실행 객체 생성
+			//사용자로부터 요구 받은 쿼리 실행
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1,id);
+			
+			
+			ResultSet rs = st.executeQuery();
+
+
+							
+			if(rs.next()) {
+				int nid = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				Date regdate = rs.getDate("REGDATE");
+				String writerid = rs.getString("WRITER_ID");
+				String hit = rs.getString("HIT");
+				String files = rs.getString("FILES");
+				String content = rs.getString("CONTENT");
+				
+				//notice객체그릇에 데이터(속성들)담기, 생성자와 순서 동일하게
+				notice = new Notice(
+									nid,
+									title,
+									regdate,
+									writerid,
+									hit,
+									files,
+									content
+									);
+				
+				}
+		    	rs.close();
+		    	st.close();
+		        con.close();
+		        
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return notice;
 	}
 	
 	
