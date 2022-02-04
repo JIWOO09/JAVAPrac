8838472d5268ac89968900d8a74b08b5e794da0f
JIWOO <s__009@naver.com>
2022-02-04 17:35:02 +0900

    2022.02.04 파일업로드

--- JspProject/src/main/java/com/jiwoo/web/controller/admin/notice/RegController.java
+++ JspProject/src/main/java/com/jiwoo/web/controller/admin/notice/RegController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.jiwoo.web.entity.Notice;
 import com.jiwoo.web.service.NoticeService;
 
-
+@MultipartConfig(
+		//location="/tmp", 임시 저장 디렉토리 지정 설정안하는게 나음
+		fileSizeThreshold=1024*1024, //메모리에 저장되는 임시파일 크기 지정
+		maxFileSize=1024*1024*5,//업로드할 1개의 파일 최대크기지정
+		maxRequestSize=1024*1024*5*5//request시 최대 크키 지정
+		)
 @WebServlet("/admin/board/notice/reg")
 public class RegController extends HttpServlet {
 	
