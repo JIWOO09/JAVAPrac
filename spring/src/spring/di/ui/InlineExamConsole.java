85b4010e6f17f5d1e37f5d35acc643631fecb755
JIWOO <s__009@naver.com>
2022-03-20 19:15:56 +0900

    2022.03.20 어노테이션 DI

---
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -0,0 +1,31 @@
+package spring.di.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.di.entity.Exam;
+
+public class InlineExamConsole implements ExamConsole {
+
+	public InlineExamConsole() {
+	}
+	private Exam exam;
+	
+	//생성자를 통해서 물려받는
+	public InlineExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+
+	@Override
+	public void print() {
+		System.out.printf("total : %d, avg : %f\n", exam.total(), exam.avg());
+		
+	}
+
+	@Autowired
+	@Override
+	public void setExam(Exam exam) {
+		this.exam = exam;
+		
+	}
+
+}
