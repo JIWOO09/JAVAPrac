eb0949d89037ede30fb1c69c00c473ea4297e510
JIWOO <s__009@naver.com>
2022-03-27 22:06:23 +0900

    2022.03.27 경로 코드를 중복되지 않게 ViewResolver 사용

--- webpj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webpj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -6,17 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+
 //인터페이스 구현
 public class IndexController implements Controller {
 
+	
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("index contriller");
 		//담기
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("data", "Hi MVC");
 		//컨트롤를 통해야만 페이지 볼 수 있다.
-		mv.setViewName("/WEB-INF/view/index.jsp");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
 		
 		return mv;
 	}
